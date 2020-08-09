c0949709bb174f076a4f1e2592113e193f00ea5d
koushack <67109331+koushack@users.noreply.github.com>
2020-08-09 17:37:37 +0530

    Update pse.js

--- pse.js
+++ pse.js
@@ -1,1 +1,12 @@
+var app = angular.module('WikiApp', ['ngAnimate']);
+app.controller('MainCtrl', function($scope, $http, $timeout) {
+  var form = $('form');
+  var close = $('.eks');
+  var input = $('input');
+  var search = $("#search");
+  var help = $("#help");
+  
+  $scope.results = [];
 
+  close.on('click', function() {
+    form.toggleClass('open');
